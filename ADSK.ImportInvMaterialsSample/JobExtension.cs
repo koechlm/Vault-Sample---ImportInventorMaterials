b450c047793c2afc39ffcf820c758843fe305187
MKoe VisualStudio <markus.koechl@autodesk.com>
2018-09-05 15:13:47 +0200

    Add project files.

---
+++ ADSK.ImportInvMaterialsSample/JobExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Connectivity.Extensibility.Framework;
+using Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities;
+using Autodesk.Connectivity.JobProcessor.Extensibility;
+using Autodesk.Connectivity.WebServices;
+
+[assembly: ApiVersion("12.0")]
+[assembly: ExtensionId("494c7aba-9cf2-4d8f-a3ca-75947a3a4cc4")]
+
+
+namespace API_Onboarding_Custom_Job
+{
+    public class JobExtension : IJobHandler
+    {
+        private static string JOB_TYPE = "YourCompany.JobName";
+
+        #region IJobHandler Implementation
+        public bool CanProcess(string jobType)
+        {
+            return jobType == JOB_TYPE;
+        }
+
+        public JobOutcome Execute(IJobProcessorServices context, IJob job)
+        {
+            try
+            {
+                MessageBox.Show("Hello World", "Job-Template-Messenger", MessageBoxButtons.OK);
+                return JobOutcome.Success;
+            }
+            catch (Exception ex)
+            {
+                context.Log(ex, "Job-Template Job failed: " + ex.ToString() + " ");
+                return JobOutcome.Failure;
+            }
+
+        }
+
+        public void OnJobProcessorShutdown(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorSleep(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorStartup(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorWake(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+        #endregion IJobHandler Implementation
+    }
+}
---
+++ ADSK.ImportInvMaterialsSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("API Onboarding - Custom Job")]
+[assembly: AssemblyDescription("API Introduction Sample 02 - Custom Job")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk")]
+[assembly: AssemblyProduct("Autodesk Vault Professional - API Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Autodesk, Markus Koechl")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("494c7aba-9cf2-4d8f-a3ca-75947a3a4cc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2019.24.0.0")]
+[assembly: AssemblyFileVersion("2019.24.0.0")]
